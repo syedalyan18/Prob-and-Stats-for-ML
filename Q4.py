7fe40e81454fdd0ff3b08f895d9bf1d2851a7b69
0648-Alyan <k250648@nu.edu.pk>
2026-07-15 19:21:33 +0500

    INITIAL COMMIT

--- Q4.py
+++ Q4.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 from scipy.stats import uniform,norm,poisson,binom
 import numpy as np
-
+import seaborn as sns
 # Gaussian Distribution
 x=np.linspace(-4,4,100)
 plt.plot(x,norm.pdf(x,loc=0,scale=1),label="Gaussian (u=0,s=1)")
@@ -15,6 +15,11 @@ plt.bar(x,binom.pmf(x,n,p),alpha=0.7,label="Binomial (n=10,p=0.5)")
 lam=3
 x=np.arange(0,10)
 plt.bar(x,poisson.pmf(x,lam),alpha=0.7,label="Poisson (l=3)")
+
+
+# Uniform Distribution
+x=np.random.uniform(low=0,high=10,size=100)
+sns.histplot(x,kde=True,label="Uniform",color="red")
 
 plt.title("Probability Distribution ")
 plt.legend()
