2b58a15a7675ae2b5128860cb0bd5e5fe500b9d7
0648-Alyan <k250648@nu.edu.pk>
2026-07-15 18:29:59 +0500

    INITIAL COMMIT

--- Q3.py
+++ Q3.py
@@ -1,11 +1,20 @@
 import matplotlib.pyplot as plt
 from scipy.stats import uniform
+import numpy as np
 
-outcomes=[1,2,3,4,5,6]
-probability=[1/6]*6
+# outcomes=[1,2,3,4,5,6]
+# probability=[1/6]*6
 
-plt.bar(outcomes,probability,color="blue",alpha=0.7)
-plt.title("PMF of a Dice Roll")
-plt.xlabel("Outcomes")
-plt.ylabel("Probability")
+# plt.bar(outcomes,probability,color="blue",alpha=0.7)
+# plt.title("PMF of a Dice Roll")
+# plt.xlabel("Outcomes")
+# plt.ylabel("Probability")
+# plt.show()
+
+x=np.linspace(0,1,100)
+pdf=uniform.pdf(x,loc=0,scale=1)
+plt.plot(x,pdf,color="orange")
+plt.title("PDF of Uniform(0,1)")
+plt.xlabel("x")
+plt.ylabel("f(x)")
 plt.show()
